1a3708be5e5093c9eaf94b56caf544f443aa538b
Willin Wang <willin@willin.org>
2016-05-10 09:54:40 +0800

    remove fs

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 'use strict';
-var fs = require('fs');
+var current = 'en';
+var motto = require('./lang/en');
 
 module.exports = function(lang) {
     lang = lang || 'en';
-    var path = __dirname + '/lang/' + lang + '.js';
-    var exists = fs.existsSync(path);
-    var motto = exists ? require('./lang/' + lang) : require('./lang/en');
+    if(lang !== current) {
+      try {
+        current = 'lang';
+        motto = require('./lang/' + lang);
+      } catch (e) {
+        console.error(e);
+      }
+    }
     return motto[Math.floor(Math.random() * (motto.length))];
 };
